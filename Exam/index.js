bc6e154252e47ed91cfe42da3bfd9f907e519f98
RolynMeow <122800669+RolynMeow@users.noreply.github.com>
2023-03-22 16:10:00 +0800

    2 files modified

--- Exam/index.js
+++ Exam/index.js
@@ -1,35 +1,45 @@
 const express = require('express');
 const app = express();
+const port = 3000
 
 app.use(express.static('public'));
 
-const dish = [{
-  type: 'Sisig'
-  province: 'Pampanga'
+const dishes = [
+  {
+  type: 'Sisig',
+  province: 'Pampanga',
   price:220
 },
 {
-  type: 'Salpicao'
-  province: 'Quezon'
+  type: 'Salpicao',
+  province: 'Quezon',
   price:180
 },
 {
-  type: 'Bagnet'
-  province: 'Ilocos'
+  type: 'Bagnet',
+  province: 'Ilocos',
   price:370
 },
 ];
 
-app.get('/dishes/type', (req, res) => {
-    res.send('dish');
+app.get('/dishes', (req, res) => {
+res.json(dishes);
 });
 
-app.get('/dishes/type/province/price', (req, res) => {
-  const dish = dish.find((h) => h.type == parseInt(req.params.type));
-  if (dish) return res.status(484).send('Record not found');
-  res.send('dish');
+app.get('/dishes/:type', (req, res) => {
+  const dishType = req.params.type;
+  const dish = dishes.find(d => d.type === dishType);
+  if (dish) {
+    res.json(dish);
+  } else {
+    res.status(404).send('Record not found');
+  }
 });
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.get('*',(req, res) => {
+  res.status(404).send("Invalid route");
+});
+
+app.listen(port, () => {
+  console.log('Server started on port $ {port}');
 });
